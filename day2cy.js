46a347fff3094a5c84adaf34c40465f7692f2802
Surabhi S <133219733+surabhi2005@users.noreply.github.com>
2024-08-31 10:21:14 +0530

    Add files via upload

---
+++ day2cy.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Smile() {
+  
+
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <h1>Smile Component</h1>
+        <br></br>
+        <p>It is a file Component</p>
+      <img src="/smile_image.jpeg" alt="Smiling Face" style={{ width: '500px', height: '500px' }} />
+     
+    </div>
+  );
+}
+export default Smile;
