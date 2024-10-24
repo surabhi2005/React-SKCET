39c3ba5946af4c80cf71e8f96ed8d67eb0f73e8b
Surabhi S <133219733+surabhi2005@users.noreply.github.com>
2024-10-24 11:01:31 +0530

    Add files via upload

---
+++ Day5/day5cw1.js
@@ -0,0 +1,19 @@
+// List.js
+import React from 'react';
+
+const List = () => {
+  const fruits = ['Apple', 'Banana', 'Cherry'];
+
+  return (
+    <div>
+      <h2>Fruit List</h2>
+      <ul>
+        {fruits.map((fruit) => (
+          <li>{fruit}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
